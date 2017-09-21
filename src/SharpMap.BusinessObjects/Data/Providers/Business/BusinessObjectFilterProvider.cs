248ff44faf3f3fd1c76d2475ae523e0afdf5ab41
TT <terribletum@gmail.com>
2017-09-21 22:53:36 +0700

    1 - locking for InMemoryBusinessObjectSource      add method InserFeature to support efficient incremental updates to InMemoryBusinessObjectSource 2 - implement FilterDelegate on BusinessObjectProvider, similar to GeometryFeatureProvider

---
+++ src/SharpMap.BusinessObjects/Data/Providers/Business/BusinessObjectFilterProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SharpMap.Data.Providers.Business
+{
+    public abstract class BusinessObjectFilterProvider
+    {
+        public delegate bool FilterMethod(object bo);
+        public FilterMethod FilterDelegate { get; set; }
+    }
+}
