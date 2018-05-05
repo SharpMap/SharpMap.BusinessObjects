3b3421d2503cd8ef707ff3aac7d11cc7c4b7776d
Tim Cook <terribletum@gmail.com>
2018-05-05 12:40:30 +0700

    Revert access modifier on _source to Private

--- src/SharpMap.BusinessObjects/Data/Providers/Business/BusinessObjectProvider.cs
+++ src/SharpMap.BusinessObjects/Data/Providers/Business/BusinessObjectProvider.cs
@@ -140,7 +140,7 @@ namespace SharpMap.Data.Providers.Business
                 GetPublicMembers(t.BaseType, collection);
         }
 
-        protected readonly IBusinessObjectSource<TFeature> _source;
+        private readonly IBusinessObjectSource<TFeature> _source;
 
         /// <summary>
         /// Creates an instance of this class
